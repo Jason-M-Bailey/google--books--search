3bcdb2c53432cf60e5375e5bde614f8ef553b23d
Jason Bailey <jmbailey816@gmail.com>
2021-09-23 21:12:28 -0400

    Routes

---
+++ routes/api/books.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const booksController = require("../../controllers/booksController");
+
+// matches with "/api/books"
+router.route("/")
+  .get(booksController.findAll)
+  .post(booksController.save);
+
+// matches with "/api/books/:id"
+router.route("/:id")
+  .get(booksController.findOne)
+  .delete(booksController.remove);
+
+module.exports = router;
