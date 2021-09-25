666170d3180b642b4df4f0ebdf5e81c0db38d2ab
Jason Bailey <jmbailey816@gmail.com>
2021-09-25 10:09:15 -0400

    updated models

--- models/book.js
+++ models/book.js
@@ -2,14 +2,32 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-  id: { type: String, require: true },
-  title: { type: String, require: true },
-  authors: [{ type: String, require: true }],
-  description: { type: String, require: true },
-  image: { type: String, unique: true, dropDups: true },
-  link: { type: String, required: true },
+  googleId: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  authors: {
+    type: Array,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  href: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+  },
 });
 
-const GoogleBooks = mongoose.model("GoogleBooks", bookSchema);
-
-module.exports = GoogleBooks;
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
