ac19462be1a129f6cf8609edffb89b85fedce519
Jason Bailey <jmbailey816@gmail.com>
2021-09-25 10:05:09 -0400

    Updated server.js to connect to mongodb

--- server.js
+++ server.js
@@ -1,29 +1,39 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
-const app = express();
+const path = require("path");
 const PORT = process.env.PORT || 3001;
+const app = express();
+const apiRoutes = require("./routes/api-routes");
 
-// define middleware here
+// Define middleware here
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// static assets (heroku)
+// Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-// define API routes
-app.use(routes);
+// Define API routes here
+app.use("/api", apiRoutes);
 
-// connect to Mongo DB
-var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
+// Connect to MongoDB
+const MONGODB_URI =
+  process.env.MONGODB_URI || "mongodb://localhost/googlebooks";
 
 mongoose.connect(MONGODB_URI, {
-  useUnifiedTopology: true,
   useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+// Send every other request to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`API server now listening on port ${PORT}!`);
+  console.log(`API server running on port ${PORT}!`);
 });
